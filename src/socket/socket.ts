71f051b8b9406560c49a963375ad40a7067d3e9d
minhle <minhldse181522@fpt.edu.vn>
2025-04-02 16:04:25 +0700

    enhance WebSocket connection: configure transport options, set reconnection attempts, and delay

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,7 +2,11 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_DB_HOST_API;
 
-const socket = io(SOCKET_URL);
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"], // Không dùng polling, bắt buộc WebSocket
+  reconnectionAttempts: 10, // Cố gắng reconnect tối đa 10 lần
+  reconnectionDelay: 5000, // Mỗi lần cách nhau 5 giây
+});
 socket.on("connect", () => {
   console.log("Connected to WebSocket server");
 });
